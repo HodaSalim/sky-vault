7a40a5097670aff729a3b5d15e985624139305e6
Hoda Salim <phoenix@Hodas-MacBook-Air.local>
2024-10-15 02:42:22 +0300

    [UI] feat: responsive header with links to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get "users/new"
   get "static_pages/home"
-  get "static_pages/help"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
   devise_for :users
   resources :workspaces
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
